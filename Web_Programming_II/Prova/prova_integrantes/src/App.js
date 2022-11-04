feb9cc18e85f8ea29bc6419968ee0ea883e0ff8e
GermanoTischler <germanotischler@gmail.com>
2022-11-03 22:26:37 -0300

    Fninished Activity

--- Web_Programming_II/Prova/prova_integrantes/src/App.js
+++ Web_Programming_II/Prova/prova_integrantes/src/App.js
@@ -6,13 +6,27 @@ function App() {
 
   return (
     <div className="App">
-      <h1 id='nome'>Germano Roberto Bortolini Tischler</h1>
-      <img id="image" src={foto_perfil}/>
-      <p id='p'>Crie um programa para ajudar o amigo marombeiro, deve ter uma tela para cadastrar o exercício <br/>
-        (nome do exercício, quantidade de series e repetições) e uma opção para marcar qual ele já concluiu.</p>
-    </div>
-    
 
+      
+      
+      <div class="tooltip">
+        <img id="img" alt="" title="Germano" src={foto_perfil}/>
+      </div>
+
+      <div>
+
+        <header>
+          <h1 id='nome'>Germano Roberto Bortolini Tischler</h1>
+        </header>
+
+        <footer>
+          <p id='p'>Crie um programa para ajudar o amigo marombeiro, deve ter uma tela para cadastrar o exercício <br/>
+          (nome do exercício, quantidade de series e repetições) e uma opção para marcar qual ele já concluiu.</p>
+        </footer>
+
+      </div>
+
+    </div>
   );
 }
 
